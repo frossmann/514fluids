102c5883583c7ce755befdf32a5d132fce042108
Francis Rossmann <frossmann@eoas.ubc.ca>
2022-03-14 19:26:50 -0700

    add Integrator() class and repackage FTCS loop

--- freeze_thaw/main.py
+++ freeze_thaw/main.py
@@ -1,4 +1,5 @@
 #%%
+from operator import ne
 import numpy as np
 import matplotlib.pyplot as plt
 from numba import jit, njit
@@ -11,10 +12,25 @@ from collections import namedtuple
 
 
 @njit
-def _update_T(last_step, dx, dy, nx, ny, dt, k_heat):
-    next_step = np.zeros_like(last_step)
+def _update_T(
+    last_step,
+    dx,
+    dy,
+    nx,
+    ny,
+    dt,
+    k_heat,
+):
+    """Forward time centered space numerical scheme to calculate
+    2D heat diffusion in a plane:"""
+    # copy the time step specifically to pull out the top and bottom
+    # boundary conditions (FIXME)
+    next_step = last_step.copy()
     for i in np.arange(1, ny - 1):
         for j in np.arange(0, nx):
+            # Set up periodic boundary conditions (note we don't need to do this if
+            # j == 0 because then j-1 == -1 which python indexes as the last element
+            # so wrapping is already taken care of )
             if j == nx - 1:
                 next_step[i, j] = (
                     last_step[i, j]
@@ -23,6 +39,7 @@ def _update_T(last_step, dx, dy, nx, ny, dt, k_heat):
                     + (dt * k_heat / (dy ** 2))
                     * (last_step[i, j - 1] - 2 * last_step[i, j] + last_step[i, 0])
                 )
+            # otherwise just step through time:
             else:
                 next_step[i, j] = (
                     last_step[i, j]
@@ -35,17 +52,25 @@ def _update_T(last_step, dx, dy, nx, ny, dt, k_heat):
 
 
 class Integrator:
+    """Integrator class to handle the bulk of the freeze-thaw model"""
+
     def check_init(self):
+        """Function to check that the stability criterion for a
+        FTCS numerical scheme is met, if not, defaults to the smallest
+        allowable time-step where the scheme becomes stable."""
         stability_crit = np.min((self.dx, self.dy)) ** 2 / (4 * self.tempvars.k_heat)
-        # if self.dt > stability_crit:
-        #     print("Warning! Unstable timestep")
-        #     print(f"{self.dt=} > {stability_crit=}")
-        #     print(f"Defaulting to minimum timestep {stability_crit=}")
-        # else:
-        #     print("Timestep is stable: ")
-        #     print(f"{self.dt=} < {stability_crit=}")
+        if self.dt > stability_crit:
+            print("Warning! Unstable timestep")
+            print(f"{self.dt=} > {stability_crit=}")
+            print(f"Defaulting to minimum timestep {stability_crit=}")
+            self.dt = stability_crit
+        else:
+            print("Timestep is stable: ")
+            print(f"{self.dt=} < {stability_crit=}")
 
     def set_init(self):
+        """Method to set up the physical parameters of the problem, integration
+        parameters and others."""
         timevars = namedtuple("timevars", self.config["timevars"].keys())
         self.timevars = timevars(**self.config["timevars"])
         gridvars = namedtuple("gridvars", self.config["gridvars"].keys())
@@ -62,6 +87,8 @@ class Integrator:
         self.check_init()
 
     def build_grid(self):
+        """Method to construct a NxM model domain given requested
+        parametrs."""
         grid = self.gridvars
         ground = self.groundvars
         temp = self.tempvars
@@ -74,11 +101,21 @@ class Integrator:
 
         #% Initialize temperatures with initial conditions:
         # Temperature grid:
-        T = np.zeros((grid.nx, grid.ny, tm.nt))
+        T = np.zeros((grid.nx, grid.ny, tm.nt))  # temperatures grid
+        M = np.ones((grid.nx, grid.ny, tm.nt))  # mask grid
+        M[: grid.n_atmos, :, :] = np.zeros(
+            (grid.nx, grid.n_atmos)
+        )  # uppermost n_atmos cells are 'empty'
+        M[int(grid.ny * ground.h_stone) :, :, :] *= 2
 
-        T0_1d = np.append(temp.T_a, np.linspace(temp.T_g, temp.T_b, grid.nx - 1))
-        T0_2d = (T0_1d * np.ones_like(X)).T
-        T[:, :, 0] = T0_2d
+        # Set the first  'plane' of initial conditions:
+        # 1D linear temperature profile with depth:
+        T0_1d = np.append(
+            temp.T_a * np.ones(grid.n_atmos),
+            np.linspace(temp.T_g, temp.T_b, grid.nx - 1),
+        )
+        # Map to 2D and set to first timestep:
+        T[:, :, 0] = (T0_1d * np.ones_like(X)).T
 
         # Boundary conditions on Temperature:
         # Bottom row for all time slices is bottom of active layer:
@@ -91,6 +128,7 @@ class Integrator:
             )  # FIXME: atmospheric temperature varies
 
         self.T = T
+        self.M = M
 
     def __init__(self, coeff_filename):
         with open(coeff_filename, "rb") as f:
@@ -100,32 +138,56 @@ class Integrator:
         self.build_grid()
 
     def update_T(self):
-        return _update_T(
+        """Wrapper function for _update_T() function which is
+        outside of the Integrator class which uses Numba's no-python
+        just-in-time compilation (~20x speedup last I checked compared
+        to a serial loop with standard compilation)."""
+        next_step = _update_T(
             self.last_step,
             self.dx,
             self.dy,
-            self.nx,
-            self.ny,
+            self.gridvars.nx,
+            self.gridvars.ny,
             self.dt,
             self.tempvars.k_heat,
         )
+        return next_step
+
+    def update_mask(self):
+        """Method which will handle tracking the occupancy of each cell,
+        given as:
+        - 0: void
+        - 1: stonme
+        - 2: soil
+        - 3: ice
+        - 4: void
+        """
+        next_mask = np.ones_like(self.M[:, :, 0])
+        next_mask[self.last_mask == 0] = self.T_a
 
     def timeloop(self):
+        """Method which holds the main timeloop."""
         t = self.timevars
         for n in range(t.nt - 1):
             self.last_step = self.T[:, :, n]
             self.T[:, :, n + 1] = self.update_T()
+            # FIXME: after delta_T from the last time step is calculated:
+            # find cells in T that have w > 0 and T < 0
+            # check if frozen
+            # juggle cells
 
 
 if __name__ == "__main__":
     tst = Integrator("uservars.yaml")
     tst.build_grid()
     tst.timeloop()
+
+    # make a plot:
     nt = tst.timevars.nt
-    time_idx = [0, int(nt / 10), int(nt / 2), int(n)]
+    time_idx = [0, int(nt / 10), int(nt / 2), int(nt - 1)]
     clim = [-5, 5]
     for ij in range(len(time_idx)):
-        fig = plt.figure(ij, figsize=(11, 7), dpi=100)
+        fig = plt.figure(ij, dpi=100)
         plt.gca().invert_yaxis()
         ax = fig.gca()
         ax.set_xlabel("Length")
