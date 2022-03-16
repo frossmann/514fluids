b410b9c0c1d9874d193b20cf9d6e0880b330c7fe
Francis Rossmann <frossmann@eoas.ubc.ca>
2022-03-15 17:29:34 -0700

    create base Integrator() base class, add Integrator2D() subclass

--- freeze_thaw/Integrators.py
+++ freeze_thaw/Integrators.py
@@ -5,61 +5,24 @@ import yaml
 from numba import jit, njit
 
 from StringUtils import tprint
-
-
-@njit
-def _update_T(
-    last_step,
-    last_mask,
-    dx,
-    dy,
-    nx,
-    ny,
-    dt,
-    k_heat,
-):
-    """Forward time centered space numerical scheme to calculate
-    2D heat diffusion in a plane:"""
-    # copy the time step specifically to pull out the top and bottom
-    # boundary conditions (FIXME)
-    next_step = last_step.copy()
-    # start the loop from the top of the simulation but continue
-    # if the cell is masked as atmospheric (or void)
-    # always skip the last step because the lower boundary is fixed...
-    for i in np.arange(0, ny - 1):
-        for j in np.arange(0, nx):
-            # Check if the [i, j]'th cell is atmospheric:
-            if last_mask[i, j] == 0:
-                # If cell is air, T[i,j] should equal T_a and
-                # we can do nothing here. NOTE: a sanitization step
-                # is necessary higher in this stack to check that atmospheric
-                # cells aren't accumulating heat.
-                continue
-            # Set up periodic boundary conditions (note we don't need to do this if
-            # j == 0 because then j-1 == -1 which python indexes as the last element
-            # so wrapping is already taken care of )
-            if j == nx - 1:
-                next_step[i, j] = (
-                    last_step[i, j]
-                    + (dt * k_heat / (dx ** 2))
-                    * (last_step[i - 1, j] - 2 * last_step[i, j] + last_step[i + 1, j])
-                    + (dt * k_heat / (dy ** 2))
-                    * (last_step[i, j - 1] - 2 * last_step[i, j] + last_step[i, 0])
-                )
-            # otherwise just step through time:
-            else:
-                next_step[i, j] = (
-                    last_step[i, j]
-                    + (dt * k_heat / (dx ** 2))
-                    * (last_step[i - 1, j] - 2 * last_step[i, j] + last_step[i + 1, j])
-                    + (dt * k_heat / (dy ** 2))
-                    * (last_step[i, j - 1] - 2 * last_step[i, j] + last_step[i, j + 1])
-                )
-    return next_step
+from calculations import _update_T, _update_T_3D
 
 
 class Integrator:
-    """Integrator class to handle the bulk of the freeze-thaw model"""
+    """Integrator class to handle the bulk of the freeze-thaw model.
+    Inherit from this base class and supply your own derivative method,
+    initialization routines and timeloop."""
+
+    def __init__(self, coeff_filename):
+        with open(coeff_filename, "rb") as f:
+            config = yaml.safe_load(f)
+        self.config = config
+
+
+class Integrator2D(Integrator):
+    """Integrator class to handle the bulk of the freeze-thaw model.
+    Inherit from this base class and supply your own derivative method,
+    initialization routines and timeloop."""
 
     def check_init(self):
         """Function to check that the stability criterion for a
@@ -147,9 +110,7 @@ class Integrator:
         self.M = M
 
     def __init__(self, coeff_filename):
-        with open(coeff_filename, "rb") as f:
-            config = yaml.safe_load(f)
-        self.config = config
+        super().__init__(coeff_filename)
         self.set_init()
         self.build_grid()
 
@@ -195,3 +156,9 @@ class Integrator:
             # find cells in T that have w > 0 and T < 0
             # check if frozen
             # juggle cells
+
+
+class Integrator3D(Integrator):
+    def __init__(self, coeff_filename):
+        super().__init__(coeff_filename)
+        self.set_yinit()
