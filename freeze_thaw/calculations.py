61ba6868cfdbbf0d49ee9786be4d66203e0085b9
Francis Rossmann <frossmann@eoas.ubc.ca>
2022-03-22 17:46:38 -0700

    add scaffolding for freezeup

--- freeze_thaw/calculations.py
+++ freeze_thaw/calculations.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from numba import njit, jit
+from sklearn.svm import OneClassSVM
 
 
 @njit
@@ -178,54 +179,32 @@ def _update_T(
     return next_step
 
 
-class GridCell:
-    def __init__(self):
-        self.void = False
-        self.frozen = False
-        self.n_stones = []
-        self.n_soil = []
-        self.w = []
-        self.temp = []
-        self.last_temp = []
-
-    def set_domain(self):
-        if self.n_stones == 1:
-            if self.n_soil == 1:
-                self.domain = "soil"
-            else:
-                self.domain = "stone"
-
-    def get_latent_heat(self, delta_T):
-        """At zero degrees, some delta_T is incurred as the heat
-        equation is stepped through time. Convert delta_T to
-        the equivalent energy input based on the specific heat
-        capacity of the cell.
-        """
-        pass
-
-    def check_frozen(self, delta_T):
-        """Cell is considered 'frozen' when
-        - w = 0: water content is zero
-        - T <= 0: temperature is freezing
-        """
-        # stone cells can't freeze:
-        if self.domain == "stone":
-            return
-
-        if self.last_temp + delta_T <= 0:
-            if self.w == 0:
-                self.frozen = True
-            else:
-                joules = self.get_latent_heat(delta_T)
+def get_latent_heat(mass_h20):
+    """Returns latent heat that would be released
+    by freezing `mass_h20` of water."""
+    heat_of_fusion = 333.55 * 1000  # J/g * 1000g/1kg  [J/kg]
+    return mass_h20 * heat_of_fusion
 
 
-# #%% Spatial parameters ::
-# # make a 2d grid
-# dx = 0.1  # grid spacing
-# L = 10  # 20m in Kessler
-# N = int(L // dx)  # number of grid cells per dimension
-# x = np.linspace(0, L, N)  # 1d axis
-# X, Y = np.meshgrid(x, x)  # 2d coordinates
+def joules_from_delta_T(mass_h20, delta_temp):
+    """Returns joules released that would theoretically
+    be released by dropping a `mass_h20`
+    by `delta-temp` degrees"""
+    specific_heat_h20 = 4182  # J/(kgC)
+    return specific_heat_h20 * mass_h20 * delta_temp
 
 
-#%%# %%
+def lens_creation():
+    pass
+
+
+def find_surf_path():
+    pass
+
+
+def push_to_surface():
+    pass
+
+
+def push_to_void():
+    pass
